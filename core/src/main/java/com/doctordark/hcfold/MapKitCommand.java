a9a6ab35e75e1c720bc66120460e2f1baa7eda3a
DoctorDark <doctordark11@gmail.com>
2015-11-22 18:02:32 +0000

    Don't create a new inventory every time /mapkit is run, make stuff easier to read

--- core/src/main/java/com/doctordark/hcfold/MapKitCommand.java
+++ core/src/main/java/com/doctordark/hcfold/MapKitCommand.java
@@ -13,6 +13,7 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
+import org.bukkit.craftbukkit.libs.joptsimple.internal.Strings;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.HumanEntity;
 import org.bukkit.entity.Player;
@@ -27,27 +28,23 @@ import org.bukkit.potion.Potion;
 import org.bukkit.potion.PotionType;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 public class MapKitCommand implements CommandExecutor, TabCompleter, Listener {
 
-    private final Set<Inventory> tracking = new HashSet<>();
+    private static final String SEPARATOR_LINE = ChatColor.GRAY.toString() + ChatColor.STRIKETHROUGH.toString() + ChatColor.BOLD.toString() + Strings.repeat('-', 16);
+
+    private Inventory mapkitInventory;
 
     public MapKitCommand(HCF plugin) {
         plugin.getServer().getPluginManager().registerEvents(this, plugin);
+
+        this.reloadMapKitInventory();
     }
 
-    @Override
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (!(sender instanceof Player)) {
-            sender.sendMessage(ChatColor.RED + "This command is only executable by players.");
-            return true;
-        }
-
+    private void reloadMapKitInventory() {
         List<ItemStack> items = new ArrayList<>();
 
         for (Enchantment enchantment : Enchantment.values()) {
@@ -57,29 +54,41 @@ public class MapKitCommand implements CommandExecutor, TabCompleter, Listener {
             }
 
             ItemBuilder builder = new ItemBuilder(Material.ENCHANTED_BOOK);
-            builder.displayName(ChatColor.YELLOW + Lang.fromEnchantment(enchantment) + ": " + ChatColor.GREEN + maxLevel);
+            builder.displayName(ChatColor.YELLOW + Lang.fromEnchantment(enchantment) + ": " + ChatColor.GREEN + (maxLevel == 0 ? "Disabled" : maxLevel));
+            builder.lore(SEPARATOR_LINE, ChatColor.WHITE + "  No Extra Data", SEPARATOR_LINE);
             items.add(builder.build());
         }
 
         for (PotionType potionType : PotionType.values()) {
+            if (potionType == PotionType.WATER) {
+                continue;
+            }
+
             Integer maxLevel = ConfigurationService.POTION_LIMITS.get(potionType);
             if (maxLevel == null) {
                 maxLevel = potionType.getMaxLevel();
             }
 
             ItemBuilder builder = new ItemBuilder(new Potion(potionType).toItemStack(1));
-            builder.displayName(ChatColor.YELLOW + WordUtils.capitalizeFully(potionType.name().replace('_', ' ')) + ": " + ChatColor.GREEN + maxLevel);
+            builder.displayName(ChatColor.YELLOW + WordUtils.capitalizeFully(potionType.name().replace('_', ' ')) + ": " + ChatColor.GREEN + (maxLevel == 0 ? "Disabled" : maxLevel));
+            builder.lore(SEPARATOR_LINE, ChatColor.WHITE + "  No Extra Data", SEPARATOR_LINE);
             items.add(builder.build());
         }
 
-        Player player = (Player) sender;
-        Inventory inventory = Bukkit.createInventory(player, InventoryUtils.getSafestInventorySize(items.size()), "Map " + ConfigurationService.MAP_NUMBER + " Kit");
-        tracking.add(inventory);
+        this.mapkitInventory = Bukkit.createInventory(null, InventoryUtils.getSafestInventorySize(items.size()), "Map " + ConfigurationService.MAP_NUMBER + " Kit");
         for (ItemStack item : items) {
-            inventory.addItem(item);
+            this.mapkitInventory.addItem(item);
+        }
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (!(sender instanceof Player)) {
+            sender.sendMessage(ChatColor.RED + "This command is only executable by players.");
+            return true;
         }
 
-        player.openInventory(inventory);
+        ((Player) sender).openInventory(this.mapkitInventory);
         return true;
     }
 
@@ -90,18 +99,15 @@ public class MapKitCommand implements CommandExecutor, TabCompleter, Listener {
 
     @EventHandler(ignoreCancelled = true, priority = EventPriority.HIGHEST)
     public void onInventoryClick(InventoryClickEvent event) {
-        if (tracking.contains(event.getInventory())) {
+        if (this.mapkitInventory.equals(event.getInventory())) {
             event.setCancelled(true);
         }
     }
 
     @EventHandler(ignoreCancelled = true, priority = EventPriority.MONITOR)
     public void onPluginDisable(PluginDisableEvent event) {
-        for (Inventory inventory : tracking) {
-            Collection<HumanEntity> viewers = new HashSet<>(inventory.getViewers());
-            for (HumanEntity viewer : viewers) {
-                viewer.closeInventory();
-            }
+        for (HumanEntity viewer : new HashSet<>(this.mapkitInventory.getViewers())) { // copy to prevent co-modification
+            viewer.closeInventory();
         }
     }
 }
