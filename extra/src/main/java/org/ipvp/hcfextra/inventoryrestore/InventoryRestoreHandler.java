4034dc9337dd7c1671f3d92f154effd3cf7ef5f5
DoctorDark <doctordark11@gmail.com>
2015-12-31 18:39:43 +0000

    Make /inv work on NPCs

--- extra/src/main/java/org/ipvp/hcfextra/inventoryrestore/InventoryRestoreHandler.java
+++ extra/src/main/java/org/ipvp/hcfextra/inventoryrestore/InventoryRestoreHandler.java
@@ -1,5 +1,6 @@
 package org.ipvp.hcfextra.inventoryrestore;
 
+import com.doctordark.hcf.combatlog.event.LoggerDeathEvent;
 import lombok.Getter;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
@@ -41,6 +42,11 @@ public class InventoryRestoreHandler implements CommandExecutor, TabCompleter, L
         public InventoryState(PlayerInventory inventory) {
             this(inventory.getContents(), inventory.getArmorContents());
         }
+    }
+
+    @EventHandler(ignoreCancelled = true, priority = EventPriority.MONITOR)
+    public void onLoggerDeath(LoggerDeathEvent event) {
+        this.lastDeathLocation.put(event.getLoggerEntity().getUniqueID(), new InventoryState(event.getLoggerEntity().getBukkitEntity().getInventory()));
     }
 
     @EventHandler(ignoreCancelled = true, priority = EventPriority.MONITOR)
