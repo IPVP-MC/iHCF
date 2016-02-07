da323f98229f66ecb92aeec1031926ab0444821e
Billy Jenkins <originmc@outlook.com>
2016-02-07 21:31:25 +0000

    Remove lightning code in DeathListener

--- core/src/main/java/com/doctordark/hcf/listener/DeathListener.java
+++ core/src/main/java/com/doctordark/hcf/listener/DeathListener.java
@@ -6,15 +6,7 @@ import com.doctordark.hcf.faction.type.Faction;
 import com.doctordark.hcf.faction.type.PlayerFaction;
 import com.doctordark.hcf.user.FactionUser;
 import com.doctordark.util.JavaUtils;
-import net.minecraft.server.v1_7_R4.EntityLightning;
-import net.minecraft.server.v1_7_R4.PacketPlayOutSpawnEntityWeather;
-import net.minecraft.server.v1_7_R4.WorldServer;
-import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.Location;
-import org.bukkit.Sound;
-import org.bukkit.craftbukkit.v1_7_R4.CraftWorld;
-import org.bukkit.craftbukkit.v1_7_R4.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -43,31 +35,18 @@ public class DeathListener implements Listener {
         Player player = event.getEntity();
         PlayerFaction playerFaction = plugin.getFactionManager().getPlayerFaction(player);
 
-        if (playerFaction != null) {
-            Faction factionAt = plugin.getFactionManager().getFactionAt(player.getLocation());
-            double dtrLoss = (1.0D * factionAt.getDtrLossMultiplier());
-            double newDtr = playerFaction.setDeathsUntilRaidable(playerFaction.getDeathsUntilRaidable() - dtrLoss);
+        if (playerFaction == null) return;
 
-            Role role = playerFaction.getMember(player.getUniqueId()).getRole();
-            long baseDelay = plugin.getConfiguration().getFactionDtrRegenFreezeBaseMilliseconds();
-            playerFaction.setRemainingRegenerationTime(baseDelay + (playerFaction.getOnlinePlayers().size() * plugin.getConfiguration().getFactionDtrRegenFreezeMillisecondsPerMember()));
-            playerFaction.broadcast(ChatColor.GOLD + "Member Death: " + plugin.getConfiguration().getRelationColourTeammate() +
-                    role.getAstrix() + player.getName() + ChatColor.GOLD + ". " +
-                    "DTR: (" + ChatColor.WHITE + JavaUtils.format(newDtr, 2) + '/' + JavaUtils.format(playerFaction.getMaximumDeathsUntilRaidable(), 2) + ChatColor.GOLD + ").");
-        }
+        Faction factionAt = plugin.getFactionManager().getFactionAt(player.getLocation());
+        double dtrLoss = (1.0D * factionAt.getDtrLossMultiplier());
+        double newDtr = playerFaction.setDeathsUntilRaidable(playerFaction.getDeathsUntilRaidable() - dtrLoss);
 
-        if (Bukkit.spigot().getTPS()[0] > 15) { // Prevent unnecessary lag during prime times.
-            Location location = player.getLocation();
-            WorldServer worldServer = ((CraftWorld) location.getWorld()).getHandle();
-
-            EntityLightning entityLightning = new EntityLightning(worldServer, location.getX(), location.getY(), location.getZ(), false);
-            PacketPlayOutSpawnEntityWeather packet = new PacketPlayOutSpawnEntityWeather(entityLightning);
-            for (Player target : Bukkit.getOnlinePlayers()) {
-                if (plugin.getUserManager().getUser(target.getUniqueId()).isShowLightning()) {
-                    ((CraftPlayer) target).getHandle().playerConnection.sendPacket(packet);
-                    target.playSound(target.getLocation(), Sound.AMBIENCE_THUNDER, 1.0F, 1.0F);
-                }
-            }
-        }
+        Role role = playerFaction.getMember(player.getUniqueId()).getRole();
+        long baseDelay = plugin.getConfiguration().getFactionDtrRegenFreezeBaseMilliseconds();
+        playerFaction.setRemainingRegenerationTime(baseDelay + (playerFaction.getOnlinePlayers().size() * plugin.getConfiguration().getFactionDtrRegenFreezeMillisecondsPerMember()));
+        playerFaction.broadcast(ChatColor.GOLD + "Member Death: " + plugin.getConfiguration().getRelationColourTeammate() +
+                role.getAstrix() + player.getName() + ChatColor.GOLD + ". " +
+                "DTR: (" + ChatColor.WHITE + JavaUtils.format(newDtr, 2) + '/' + JavaUtils.format(playerFaction.getMaximumDeathsUntilRaidable(), 2) + ChatColor.GOLD + ").");
     }
+
 }
